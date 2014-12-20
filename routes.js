6f8df29051ec234a0692d4ee37f5be49608b1e48
elisabot <elisabeth.nimtzmeyer@gmail.com>
2014-12-19 17:20:09 -0800

    additional mushrooms added

--- routes.js
+++ routes.js
@@ -4,8 +4,15 @@
 // 	layoutTemplate: 'layout'
 // });
 
+
 Router.route('/', function () {
   this.render('home');
+});
+
+//A: Oh goodness, I'm stumped.
+Router.route('/quest/mystery/possitive-id', function () {
+	this.layout('questLayout');
+	this.render('mysteryPosID');
 });
 
 //Q: Does your mushroom have
@@ -13,6 +20,8 @@ Router.route('/quest/initial', function () {
 	this.layout('questLayout');
 	this.render('questionInitial');
 });
+
+
 
 // Amanita Track//
 //Q: Do mature specimens have white, pinkish, or yellow gills?
@@ -40,6 +49,7 @@ Router.route('/quest/amanita/possitive-id', function () {
 });
 
 
+
 // Milk Cap or Russula Track//
 //Q: Is there a veil covering the young gills?
 Router.route('/quest/milkcap-russula/gills', function () {
@@ -64,6 +74,7 @@ Router.route('/quest/milkcap-russula/possitive-id', function () {
 	this.layout('questLayout');
 	this.render('mcrPosID');
 });
+
 
 
 // Miscellaneous Dark Spored Mushroom Track//
@@ -104,6 +115,7 @@ Router.route('/quest/darkspore/possitive-id', function () {
 });
 
 
+
 //Track for light spored gilled mushrooms with a ring//
 //Q: Are the spores white or perhaps greenish?
 Router.route('/quest/lightspore/spore-color', function () {
@@ -118,42 +130,126 @@ Router.route('/quest/lightspore/possitive-id', function () {
 });
 
 
-//Chantrelle and fake chantrelle track//
+
+//chanterelle and fake chanterelle track//
 //Q: Does the stem look fibrous when broken? It may also become juicy or milky when cut.
-Router.route('/quest/chantrelle/fibrous', function () {
+Router.route('/quest/chanterelle/fibrous', function () {
 	this.layout('questLayout');
 	this.render('chanFibrous');
 });
 
 //Q: Are the spores white, pinkish, yellow or lilac tinged?
-Router.route('/quest/chantrelle/spore-color', function () {
+Router.route('/quest/chanterelle/spore-color', function () {
 	this.layout('questLayout');
 	this.render('chanSporeColor');
 });
 
 //Q: Are the mature gills pale or brightly colored?
-Router.route('/quest/chantrelle/gill-color', function () {
+Router.route('/quest/chanterelle/gill-color', function () {
 	this.layout('questLayout');
 	this.render('chanGillColor');
 });
 
 //Q: Are the gills fairly thick, blunt, and usually forked or connected by cross veins?
-Router.route('/quest/chantrelle/gills', function () {
+Router.route('/quest/chanterelle/gills', function () {
 	this.layout('questLayout');
 	this.render('chanGills');
 });
 
-//A: You have stumbled on a chantrelle or false chantrelle.
-Router.route('/quest/chantrelle/possitive-id', function () {
+//A: You have stumbled on a chanterelle or false chanterelle.
+Router.route('/quest/chanterelle/possitive-id', function () {
 	this.layout('questLayout');
 	this.render('chanPosID');
 });
 
-//Teeth fungi track//
+
+
+//Agaricus track//
+//Q: Are the spores brown?
+Router.route('/quest/agaricus/spore-color', function () {
+	this.layout('questLayout');
+	this.render('agarSporeColor');
+});
+
+//Q: Are the gills deep brown but white or pink when young?
+Router.route('/quest/agaricus/gill-color', function () {
+	this.layout('questLayout');
+	this.render('agarGillColor');
+});
+
+//Q: Are the gills free from the stem?
+Router.route('/quest/agaricus/free-gill', function () {
+	this.layout('questLayout');
+	this.render('agarFreeGill');
+});
+
+//A: You may have found an Agaricus.
+Router.route('/quest/agaricus/possitive-id', function () {
+	this.layout('questLayout');
+	this.render('agarPosID');
+});
+
+
+
+// Boletes Track//
+//Q: Is the mushroom's cap fleshy?
+Router.route('/quest/boletes/cap', function () {
+	this.layout('questLayout');
+	this.render('boletesCap');
+});
+
+//Q: Does the mushroom have a mostly central stalk?
+Router.route('/quest/boletes/stalk', function () {
+	this.layout('questLayout');
+	this.render('boletesStalk');
+});
+
+//Q: Are the tubes soft and spongy?
+Router.route('/quest/boletes/texture', function () {
+	this.layout('questLayout');
+	this.render('boletesTexture');
+});
+
+//A: You are looking at some sort of Bolete.
+Router.route('/quest/boletes/possitive-id', function () {
+	this.layout('questLayout');
+	this.render('bolPosID');
+});
+
+
+
+// Polypores Track//
+//Q: Is the mushroom tough or woody?
+Router.route('/quest/polypores/cap', function () {
+	this.layout('questLayout');
+	this.render('polCap');
+});
+
+//Q: Is the mushroom growing on wood in a shelflike or bracketlike formation?
+Router.route('/quest/polypores/form', function () {
+	this.layout('questLayout');
+	this.render('polyporesForm');
+});
+
+//Q: Does the stalk grow off center and are the tubes rather tough?
+Router.route('/quest/polypores/tubes', function () {
+	this.layout('questLayout');
+	this.render('polTubes');
+});
+
+//A: That could be a Polypore.
+Router.route('/quest/polypores/possitive-id', function () {
+	this.layout('questLayout');
+	this.render('polPosID');
+});
+
+
+
+//Hynoid teeth fungi track//
 //Q: Does the mushroom have downward-pointing spines or teeth? There is a chance it may not have a well defined cap and stalk.
 Router.route('/quest/hydnoid/tooth-like', function () {
 	this.layout('questLayout');
-	this.render('hydGillColor');
+	this.render('hydTeeth');
 });
 
 //A: You are looking at some sort of teeth fungis.
@@ -162,49 +258,137 @@ Router.route('/quest/hydnoid/possitive-id', function () {
 	this.render('hydPosID');
 });
 
+
+
 //Morel and false morel track//
 //Q: Is the cap brainlike, saddle-shaped, thimble-like, or heneycombed with pits and ridges?
 Router.route('/quest/morel/cap', function () {
 	this.layout('questLayout');
-	this.render('hydGillColor');
+	this.render('morCap');
 });
 
 //Q: Is there a sack at the base of the stalk?
-Router.route('/quest/morel/cap', function () {
+Router.route('/quest/morel/veil', function () {
 	this.layout('questLayout');
-	this.render('hydGillColor');
+	this.render('morVeil');
 });
 
-//A: You are looking at some sort of teeth fungis.
+//A: You are looking at some sort of Morel or false Morel.
 Router.route('/quest/morel/possitive-id', function () {
 	this.layout('questLayout');
-	this.render('hydPosID');
+	this.render('morPosID');
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//A: Oh goodness, I'm stumped.
-Router.route('/quest/mystery/possitive-id', function () {
+//Coral and Club Fungi track//
+//Q: Is the mushroom differently shaped?
+Router.route('/quest/club-coral/shape', function () {
 	this.layout('questLayout');
-	this.render('mysteryPosID');
+	this.render('club-coralShape');
 });
+
+//Q: Does it have a stalk?
+Router.route('/quest/club-coral/stalk', function () {
+	this.layout('questLayout');
+	this.render('club-coralStalk');
+});
+
+//Q: Is there a sack at the base?
+Router.route('/quest/club-coral/sack', function () {
+	this.layout('questLayout');
+	this.render('club-coralSack');
+});
+
+//Q: Is the mushroom standing upright and clublike?
+Router.route('/quest/club-coral/upright', function () {
+	this.layout('questLayout');
+	this.render('club-coralUpright');
+});
+
+//Q: Is it coral-like(branched) with either a smooth to slightly wrinkled surface?
+Router.route('/quest/club-coral/surface', function () {
+	this.layout('questLayout');
+	this.render('club-coralSurface');
+});
+
+//A: That could be a Coral or Club Fungi.
+Router.route('/quest/club-coral/possitive-id', function () {
+	this.layout('questLayout');
+	this.render('club-coralPosID');
+});
+
+
+
+//Puffball and Earthstar Track//
+//Q: Does the mushroom have a spore case? *It may or may not have a stalk or rays.
+Router.route('/quest/puffball/spore-case', function () {
+	this.layout('questLayout');
+	this.render('puffSporeCase');
+});
+
+//Q: Is it growing on either the ground or wood?
+Router.route('/quest/puffball/habitat', function () {
+	this.layout('questLayout');
+	this.render('puffHabitat');
+});
+
+//A: You may have a puffball or earthstar.
+Router.route('/quest/puffball/possitive-id', function () {
+	this.layout('questLayout');
+	this.render('puffPosID');
+});
+
+
+
+//Chantrerelle Track//
+//Q: Is the cap and stalk jellylike?
+Router.route('/quest/chanterelle/texture', function () {
+	this.layout('questLayout');
+	this.render('chanTexture');
+});
+
+//Q: Is it growing on the ground?
+Router.route('/quest/chanterelle/habitat', function () {
+	this.layout('questLayout');
+	this.render('chanHabitat');
+});
+
+//Q: Is the underside wrinkled or veined? *In one case the cap is trumpetlike with a smooth underside.
+Router.route('/quest/chanterelle/surface', function () {
+	this.layout('questLayout');
+	this.render('chanSurface');
+});
+
+//A: That could be a chanterelle.
+// Router.route('/quest/chanterelle/possitive-id', function () {
+// 	this.layout('questLayout');
+// 	this.render('chanPosID');
+// });
+
+
+
+//Other Mushrooms//
+//A: That might be some other kind of mushroom.
+Router.route('/quest/other/possitive-id', function () {
+	this.layout('questLayout');
+	this.render('otherPosID');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
