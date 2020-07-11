676bd092def47c4791b6ca2e369bc14e6950bcf1
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2020-07-10 22:18:12 -0700

    this should not have been global ever

--- tests/messing-about.js
+++ tests/messing-about.js
@@ -9,14 +9,18 @@
 //what do i need to do in order to make a external link work?
 
 //this is the standard layout:
-flowchartRoutes = { //this is what the router connects with
-	'initial': { //this is the name used to refer to the page
-		prompt: 'What is this?', //this is the question that the user views on the page
-		options: [{ //this is a function or array for the user's selection
-			text: 'Gills', //This is the title on the button
-			slug: 'amanita-gills' //This is how we create the browser's slug
-		}]	}
+function isolateThisGarbage () {
+	let flowchartRoutes = { //this is what the router connects with
+		'initial': { //this is the name used to refer to the page
+			prompt: 'What is this?', //this is the question that the user views on the page
+			options: [{ //this is a function or array for the user's selection
+				text: 'Gills', //This is the title on the button
+				slug: 'amanita-gills' //This is how we create the browser's slug
+			}]	
+		}
+	}
 }
+
 
 //we need to send the user to a pre-existing link without creating a slug
 
